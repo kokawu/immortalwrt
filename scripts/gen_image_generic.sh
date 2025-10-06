771ea7f4d47ef5eb0dbe9ee2c94bc08c8404abd7
kokawu <kokawu05@gmail.com>
2025-10-06 13:31:17 +0800

    WIP: local changes before rebase 2025-10-06T13:31:17+0800

--- scripts/gen_image_generic.sh
+++ scripts/gen_image_generic.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # Copyright (C) 2006-2012 OpenWrt.org
 set -e -x
-if [ $# -ne 5 ] && [ $# -ne 6 ]; then
+
+if [ $# -ne 5 ] && [ $# -ne  6 ]; then
     echo "SYNTAX: $0 <file> <kernel size> <kernel directory> <rootfs size> <rootfs image> [<align>]"
     exit 1
 fi
@@ -21,12 +22,32 @@ head=16
 sect=63
 
 # create partition table
-set $(ptgen -o "$OUTPUT" -h $head -s $sect ${GUID:+-g} -t "${KERNELPARTTYPE}" -p "${KERNELSIZE}m${PARTOFFSET:+@$PARTOFFSET}" -t "${ROOTFSPARTTYPE}" -p "${ROOTFSSIZE}m" ${ALIGN:+-l $ALIGN} ${SIGNATURE:+-S 0x$SIGNATURE} ${GUID:+-G $GUID})
-
-KERNELOFFSET="$(($1 / 512))"
-KERNELSIZE="$2"
-ROOTFSOFFSET="$(($3 / 512))"
-ROOTFSSIZE="$(($4 / 512))"
+# When building a GPT disk (GUID set), add a small BIOS Boot partition (type 0xEF02)
+# before kernel/rootfs to support GRUB BIOS embedding on GPT. This preserves UEFI
+# boot and enables legacy BIOS boot without changing higher-level configs.
+if [ -n "$GUID" ]; then
+    # Upstream GPT layout: [kernel KERNELSIZE] [rootfs ROOTFSSIZE]
+    set $(ptgen -o "$OUTPUT" -h $head -s $sect -g \
+        -t "${KERNELPARTTYPE}" -p "${KERNELSIZE}m${PARTOFFSET:+@$PARTOFFSET}" \
+        -t "${ROOTFSPARTTYPE}" -p "${ROOTFSSIZE}m" \
+        ${ALIGN:+-l $ALIGN} ${SIGNATURE:+-S 0x$SIGNATURE} -G $GUID)
+    # Convert byte offsets to 512-byte sectors for dd seek/count usage
+    KERNELOFFSET="$(($1 / 512))"
+    KERNELSIZE="$2"
+    ROOTFSOFFSET="$(($3 / 512))"
+    # IMPORTANT: ROOTFSSIZE is used as a sector count when padding with dd
+    # (bs=512). Convert bytes -> sectors to avoid oversized writes.
+    ROOTFSSIZE="$(($4 / 512))"
+else
+    set $(ptgen -o "$OUTPUT" -h $head -s $sect \
+        -t "${KERNELPARTTYPE}" -p "${KERNELSIZE}m${PARTOFFSET:+@$PARTOFFSET}" \
+        -t "${ROOTFSPARTTYPE}" -p "${ROOTFSSIZE}m" \
+        ${ALIGN:+-l $ALIGN} ${SIGNATURE:+-S 0x$SIGNATURE})
+    KERNELOFFSET="$(($1 / 512))"
+    KERNELSIZE="$2"
+    ROOTFSOFFSET="$(($3 / 512))"
+    ROOTFSSIZE="$(($4 / 512))"
+fi
 
 # Using mcopy -s ... is using READDIR(3) to iterate through the directory
 # entries, hence they end up in the FAT filesystem in traversal order which
@@ -58,3 +79,4 @@ else
 fi
 dd if="$OUTPUT.kernel" of="$OUTPUT" bs=512 seek="$KERNELOFFSET" conv=notrunc
 rm -f "$OUTPUT.kernel"
+
